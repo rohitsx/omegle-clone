5879a0f0af4154bce200d1c787c2623c9e503aea
rohit bind <rohiitrb@gmail.com>
2024-06-22 17:18:41 +0530

    fixing 404 not found /chat

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import './style/index.css'
-import { HashRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-       <HashRouter><App/></HashRouter>
+       <App/>
 )
