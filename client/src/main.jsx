ee568a433e828c85d84c6f3c3eb235bf04438139
rohit bind <rohiitrb@gmail.com>
2024-06-22 17:15:24 +0530

    added hasrouter to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import './style/index.css'
+import { HashRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-       <App/>
+       <HashRouter><App/></HashRouter>
 )
